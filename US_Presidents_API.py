c1f9c6e67f133b9ef3afbced03e16f20802214be
Chris <40926430+Se7enSeconds@users.noreply.github.com>
2019-07-26 10:33:26 -0400

    Update US_Presidents_API.py

--- US_Presidents_API.py
+++ US_Presidents_API.py
@@ -7,16 +7,15 @@ from US_Presidents import US_Presidents
 app = Flask(__name__)
 
 onInstance = US_Presidents()
+data = onInstance.fetch_data()
 
 @app.route("/US_Presidents/")
 def display():
-  data = onInstance.fetch_data()
+  data['Ingestion Time'] = datetime.now().strftime('%b-%d-%Y %H:%M:%S')
   return data.to_html(index=False)
 
 @app.route("/US_Presidents/download")
 def download_csv():
-  data = onInstance.fetch_data()
-  data['Ingestion Time'] = datetime.now().strftime('%b-%d-%Y %H:%M:%S')
   response = make_response(data.to_csv(index=False))
   response.headers["Content-Disposition"] = "attachment; filename=US_Presidents.csv"
   response.headers["Content-Type"] = "text/csv"
